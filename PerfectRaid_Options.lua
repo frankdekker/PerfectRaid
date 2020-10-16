b8492a9bc9b650e8ae286c9b55caf6fbccef0e36
Frank Dekker <fdekker@gmail.com>
2020-10-16 16:53:16 +0200

    removed backdrop from PROptionsTemplate.xml

--- PerfectRaid_Options.lua
+++ PerfectRaid_Options.lua
@@ -68,19 +68,20 @@ end
 
 local tabs = {}
 function Options:CreateOptions()
-	local frame = CreateFrame("Frame", "PROptions", UIParent, BackdropTemplateMixin and "BackdropTemplate,PROptionsTemplate")
-		
+	local frame = CreateFrame("Frame", "PROptions", UIParent, "PROptionsTemplate")
 	frame:SetBackdrop({
-		bgFile = "Interface\DialogFrame\UI-DialogBox-Background",
-		tile = true, tileSize = 32,
+		bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background",
+		tile = true,
+		tileSize = 32,
+		edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Border",
+		edgeSize = 32,
 		insets = {
-			left = 5,
-			right = 5,
-			top = 5,
-			bottom = 5
-		},
-	})	
-	frame:SetBackdropColor(0,0,0,0.5)
+			left = 11,
+			right = 12,
+			top = 12,
+			bottom = 11
+		}
+	})
 	frame:SetHeight(450)
 	frame:SetWidth(650)
 	frame:SetPoint("CENTER", 0, 50)
